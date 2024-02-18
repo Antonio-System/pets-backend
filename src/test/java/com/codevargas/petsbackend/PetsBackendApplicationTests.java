73c102435ceb885f2b7f490773c41814d217781b
Pedro Vargas <pedrovargas@Pedros-MacBook-Air.local>
2024-02-17 21:38:01 -0300

    init

---
+++ src/test/java/com/codevargas/petsbackend/PetsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codevargas.petsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
